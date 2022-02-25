8b3880d656f495a1c722211aff94abd2067daf19
garyb6 <gary.bennett6@outlook.com>
2022-02-25 17:15:19 +0000

    initial commit

---
+++ src/main/java/educationalgoals/educationalgoals/EducationalgoalsApplication.java
@@ -0,0 +1,13 @@
+package educationalgoals.educationalgoals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalgoalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationalgoalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/educationalgoals/educationalgoals/EducationalgoalsApplicationTests.java
@@ -0,0 +1,13 @@
+package educationalgoals.educationalgoals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalgoalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
