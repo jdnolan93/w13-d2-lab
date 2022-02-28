4e3a73b05585a98497752846d149ec8dd2bd30d7
garyb6 <gary.bennett6@outlook.com>
2022-02-28 00:53:50 +0000

    question controller population with get all, get by id and post methods

--- src/main/java/educationalgoals/educationalgoals/controllers/QuestionController.java
+++ src/main/java/educationalgoals/educationalgoals/controllers/QuestionController.java
@@ -1,4 +1,33 @@
 package educationalgoals.educationalgoals.controllers;
 
+import educationalgoals.educationalgoals.models.Question;
+import educationalgoals.educationalgoals.repositories.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class QuestionController {
+
+    @Autowired
+    QuestionRepository questionRepository;
+
+    @GetMapping(value = "/questions")
+    public ResponseEntity<List<Question>> getAllQuestions(){
+        return new ResponseEntity<>(questionRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/questions/{id}")
+    public ResponseEntity getAnswer(@PathVariable Long id){
+        return new ResponseEntity(questionRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/questions")
+    public ResponseEntity<Question> postShip(@RequestBody Question question){
+        questionRepository.save(question);
+        return new ResponseEntity<>(question, HttpStatus.CREATED);
+    }
 }
