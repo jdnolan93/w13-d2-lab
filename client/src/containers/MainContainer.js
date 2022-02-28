7c9a11b6804e2e84dfac3b09a7ddd22db0a5b8bb
Jamie <jamienolan93@hotmail.co.uk>
2022-02-28 15:36:53 +0000

    player state can be updated in start container

--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import StartContainer from './StartContainer';
 import GameContainer from './GameContainer';
 import QuizContainer from './QuizContainer';
 
-
 const MainContainer = () => {
 
-        
+    const [playerOne, setPlayerOne] = useState("Player 1");
+    const [playerTwo, setPlayerTwo] = useState("Player 2");
+
   return (
 
     <Router>
         <Routes>
-            <Route path = "/start" element={<StartContainer/>}/>
+            <Route path = "/start" element={<StartContainer playerOne = {playerOne} playerTwo = {playerTwo} setPlayerOne = {setPlayerOne} setPlayerTwo = {setPlayerTwo}/>}/>
+            {/* <Route path = "/start" element={<StartContainer />}/> */}
             <Route path = "/quiz" element={<QuizContainer/>}/>
             <Route path = "/game" element={<GameContainer/>}/>
         </Routes>
--- client/src/containers/StartContainer.js
+++ client/src/containers/StartContainer.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
-const StartContainer = () => {
+
+const StartContainer = ({playerOne, playerTwo, setPlayerOne, setPlayerTwo}) => {
     
+    // const [playerOne, setPlayerOne] = useState("Player 1");
+    // const [playerTwo, setPlayerTwo] = useState("Player 2");
+    
+    const handleChangePlayerOne = (event) => {
+
+        setPlayerOne(event.target.value);
+
+        console.log(playerOne);
+    }
+    const handleChangePlayerTwo = (event) => {
+
+        setPlayerTwo(event.target.value);
+
+        console.log(playerTwo);
+    }
+
     return (
-        <h1>This is the start page</h1>
+        <>
+        <h1>Educational Goals</h1>
+        <input type = "text" onChange={handleChangePlayerOne} id = "Player 1 input" />  
+        <input type = "text" onChange={handleChangePlayerTwo} id = "Player 2 input"/> 
+        <a href = "/quiz">Start</a> 
+        </>
+    
     )
     
 }
