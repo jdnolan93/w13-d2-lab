3bfebd7d9f31789e1d78cd3bceb5335f1495b900
garyb6 <gary.bennett6@outlook.com>
2022-02-28 00:51:11 +0000

    answer controller populated with get all, get by id and post methods

--- src/main/java/educationalgoals/educationalgoals/controllers/AnswerController.java
+++ src/main/java/educationalgoals/educationalgoals/controllers/AnswerController.java
@@ -1,4 +1,33 @@
 package educationalgoals.educationalgoals.controllers;
 
+import educationalgoals.educationalgoals.models.Answer;
+import educationalgoals.educationalgoals.repositories.AnswerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class AnswerController {
+
+    @Autowired
+    AnswerRepository answerRepository;
+
+    @GetMapping(value = "/answers")
+    public ResponseEntity<List<Answer>> getAllAnswers(){
+        return new ResponseEntity<>(answerRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/answers/{id}")
+    public ResponseEntity getAnswer(@PathVariable Long id){
+        return new ResponseEntity(answerRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/answers")
+    public ResponseEntity<Answer> postShip(@RequestBody Answer answer){
+        answerRepository.save(answer);
+        return new ResponseEntity<>(answer, HttpStatus.CREATED);
+    }
 }
