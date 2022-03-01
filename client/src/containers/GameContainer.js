34b5224ca83548e65a66b342a5061d8d1b414797
Jamie <jamienolan93@hotmail.co.uk>
2022-03-01 00:28:00 +0000

    added information to game container

--- client/src/containers/GameContainer.js
+++ client/src/containers/GameContainer.js
@@ -8,8 +8,13 @@ const GameContainer = ({playerOne, playerTwo}) => {
         <>
         <h1>This is the game page</h1>
         <h2> {playerOne} is player one </h2>
+        <h3> This is the score counter</h3>
+        <h4> This is direction selector</h4>
         <h2> {playerTwo} is player two </h2>
-        <Link to = "/quiz">Back to Quiz</Link>
+        <h3> This is the score counter</h3>
+        <h4> This is direction selector</h4>
+        <h1> This is the penalty</h1>
+        <Link to = "/quiz">Next question</Link>
         </>
     )
     
