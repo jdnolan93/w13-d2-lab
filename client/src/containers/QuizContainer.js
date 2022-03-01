46c056e85c7da95d1ea339e9c0c7ec4d1ed37367
Jamie <jamienolan93@hotmail.co.uk>
2022-03-01 00:23:03 +0000

    added information to quiz container

--- client/src/containers/QuizContainer.js
+++ client/src/containers/QuizContainer.js
@@ -8,8 +8,14 @@ const QuizContainer = ({playerOne, playerTwo}) => {
         <>
         <h1>This is the quiz page</h1>
         <h2> {playerOne} is player one </h2>
+        <h3> This is the score counter</h3>
         <h2> {playerTwo} is player two </h2>
-        <Link to = "/game">To game</Link>
+        <h3> This is the score counter</h3>
+        <h2> This is the question </h2>
+        <Link to = "/game">Answer 1</Link>
+        <Link to = "/game">Answer 2</Link>
+        <Link to = "/game">Answer 3</Link>
+        <Link to = "/game">Answer 4</Link>
         </>
     )
     
