9d60383c85c1981b9cdbb08ef3d8f3e72008696c
Jamie <jamienolan93@hotmail.co.uk>
2022-03-01 16:37:42 +0000

    "front end linked to back end"

--- client/src/containers/QuizContainer.js
+++ client/src/containers/QuizContainer.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {Link, Routes, Router, Route} from 'react-router-dom';
+import Request from '../helpers/request';
+
 
 
 const QuizContainer = ({playerOne, playerTwo}) => {
+    const [quizzes, setQuizzes] = useState([]);
+
+    useEffect(()=>{
+        requestAll()
+    }, [])
+
+    const requestAll = function(){
+        const request = new Request();
+        request.get('http://localhost:8080/quizzes')
+        .then((data) => setQuizzes(data))
+    }
+
+    const findQuizById = function(id){
+        return quizzes.find((quiz) => {
+            return quiz.id === parseInt(id);
+        })
+    }
+
+
+
+    if(!quizzes){
+        return null
+    }
+
+
     
     return (
+
+    
+
+
         <>
         <h1>This is the quiz page</h1>
         <h2> {playerOne} is player one </h2>
